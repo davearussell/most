aa5745447bb022799636a5b818338a1d667db2e2
David Russell <dave.a.russell@gmail.com>
2024-08-17 13:15:50 +0100

    show line numbers

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import curses
+import math
 import time
 
 SCROLL_KEYS = {
@@ -10,6 +11,11 @@ SCROLL_KEYS = {
     'pgdn':   ['KEY_NPAGE', 'z', ' '],
 }
 SCROLL_MAP = {v: k for k, l in SCROLL_KEYS.items() for v in l}
+
+
+COLORS = [
+    ('grey', 249, -1),
+]
 
 
 class App:
@@ -24,8 +30,18 @@ class App:
         self.scr = None
         self.screen_width = None
         self.screen_height = None
+        self.show_line_numbers = True
         self.exiting = False
         self.status_msg = ''
+
+    def log_n_lines(self):
+        return math.ceil(math.log(len(self.lines) + 1, 10))
+
+    def init_colors(self):
+        self.colors = {}
+        for i, (name, fg, bg) in enumerate(COLORS):
+            curses.init_pair(i + 1, fg, bg)
+            self.colors[name] = curses.color_pair(i + 1)
 
     def log(self, fmt, *args):
         self.status_msg = fmt % args
@@ -46,20 +62,36 @@ class App:
     def handle_exit(self, _=None):
         self.exiting = True
 
+    def line_pos(self, line_i, pos='end'):
+        spacing = self.log_n_lines() - len(str(line_i + 1))
+        percent = (line_i + (1 if pos == 'end' else 0)) * 100 // len(self.lines)
+        return '(%3d%%) L%d %s|' % (percent, line_i + 1, ' ' * spacing)
+
     def draw_header(self):
-        left = self.path
+        left = "%s %s" % (self.line_pos(self.line_i, pos='start'), self.path)
         right = time.strftime('%H:%M:%S')
         msg = left + ' ' * (self.screen_width - len(left + right)) + right
         self.scr.addstr(0, 0, msg, curses.A_REVERSE)
+
+    def draw_line(self, i, line_i):
+        x = 0
+        if self.show_line_numbers:
+            fmt = '%%%dd  ' % (self.log_n_lines(),)
+            prefix = fmt % (line_i + 1,)
+            self.scr.addstr(i, x, prefix, self.colors['grey'])
+            x += len(prefix)
+        self.scr.addstr(i, x, self.lines[line_i])
 
     def draw_body(self):
         for i in range(self.screen_height - 2):
             line_i = self.line_i + i
             if 0 <= line_i < len(self.lines):
-                self.scr.addstr(i + 1, 0, self.lines[line_i])
+                self.draw_line(i + 1, line_i)
 
     def draw_footer(self):
-        msg = (self.status_msg + ' ' * self.screen_width)[:self.screen_width - 1]
+        msg = '%s %s%s' % (self.line_pos(self.line_i + self.screen_height - 3),
+                            self.status_msg, ' ' * self.screen_width)
+        msg = msg[:self.screen_width - 1]
         self.scr.addstr(self.screen_height - 1, 0, msg, curses.A_REVERSE)
 
     def redraw(self):
@@ -74,6 +106,8 @@ class App:
             self.handle_exit()
         elif name in SCROLL_MAP:
             self.handle_scroll(SCROLL_MAP[name])
+        elif name in 'Ll':
+            self.show_line_numbers = not self.show_line_numbers
         else:
             self.log("Got key: %r (%s)", key, name)
 
@@ -82,6 +116,7 @@ class App:
         self.scr.timeout(int(self.REDRAW_TIMEOUT_S * 1000))
         curses.curs_set(False)
         curses.use_default_colors()
+        self.init_colors()
         self.handle_resize()
         while not self.exiting:
             self.redraw()
