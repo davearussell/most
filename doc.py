a19488249bd6139dd5e2657340f84aeda0f88ea5
David Russell <dave.a.russell@gmail.com>
2024-08-18 16:47:40 +0100

    offsets

--- doc.py
+++ doc.py
@@ -11,7 +11,7 @@ class Document:
             self.fd = os.open(self.path, os.O_RDONLY)
             self.data = mmap.mmap(self.fd, 0, prot=mmap.PROT_READ)
         self.n_bytes = len(self.data)
-        self.lines = []
+        self.offsets = [-1]
         self.load()
 
     def load(self):
@@ -19,14 +19,12 @@ class Document:
         while i < self.n_bytes - 1:
             j = self.data.find(b'\n', i + 1)
             if j == -1:
-                j = len(self.data)
-            self.lines.append((i + 1, j))
+                j = self.n_bytes
+            self.offsets.append(j)
             i = j
 
     def __len__(self):
-        return len(self.lines)
+        return len(self.offsets) - 1
 
     def __getitem__(self, idx):
-        assert isinstance(idx, int)
-        lo, hi = self.lines[idx]
-        return self.data[lo:hi]
+        return self.data[self.offsets[idx] + 1 : self.offsets[idx + 1]]
