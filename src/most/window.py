57ae889bcbe544a27f42efd9c2afee3a46feaa30
David Russell <dave.a.russell@gmail.com>
2024-09-23 15:35:02 +0100

    report document percentages

--- src/most/window.py
+++ src/most/window.py
@@ -11,8 +11,19 @@ class Window:
     def body_height(self):
         return self.terminal.height - 2
 
+    def line_status(self, bottom=False):
+        if self.app.doc.n_lines == 0:
+            return '(100%) L0'
+        line_i = min(self.app.doc.n_lines - 1,
+                     self.app.line_i + bottom * (self.body_height - 1))
+        percent = (line_i + bottom) * 100 // self.app.doc.n_lines
+        line_s = str(line_i + 1)
+        padding = ' ' * (len(str(self.app.doc.n_lines)) - len(line_s))
+        return '(%3d%%) L%s%s' % (percent, line_s, padding)
+
     def draw_header(self):
-        self.terminal.text(self.app.doc.path, fill=True, invert=True)
+        lhs = "%s │ %s" % (self.line_status(), self.app.doc.path)
+        self.terminal.text(lhs, fill=True, invert=True)
         self.terminal.text(self.app.timestamp, align='right', invert=True)
 
     def draw_body(self):
@@ -25,7 +36,8 @@ class Window:
             self.terminal.text('', y=j+1, bg='light_grey', fill=True)
 
     def draw_footer(self):
-        self.terminal.text(self.app.status_msg, y=-1, fill=True, invert=True)
+        text = '%s │ %s' % (self.line_status(bottom=True), self.app.status_msg)
+        self.terminal.text(text, y=-1, fill=True, invert=True)
 
     def redraw(self):
         self.terminal.erase()
